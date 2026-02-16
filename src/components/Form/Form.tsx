c18c29e5b233ab27cfe807bdaf6a3b3b53443027
ArielEzequielPerez <ariperez@frba.utn.edu.ar>
2026-02-16 15:44:02 -0300

    first commit

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,63 @@
+import { useState, type FormEvent } from "react";
+import { countries } from "../../data/countries";
+import styles from "./Form.module.css";
+import type { SearchType } from "../../types";
+
+export default function Form() {
+  const [search, setSearch] = useState<SearchType>({
+    city: "",
+    country: "",
+  })
+
+  const [alert, setAlert] = useState("");
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSumit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (Object.values(search).includes("")) {
+      setAlert("Todos los campos son obligatorios");
+      return;
+    }
+  };
+  return (
+    <form className={styles.form} onSubmit={handleSumit}>
+      <div className={styles.field}>
+        <label htmlFor="city">Ciudad: </label>
+        <input
+          id="city"
+          name="city"
+          type="text"
+          placeholder="ciudad"
+          value={search.city}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className={styles.field}>
+        <label htmlFor="country">Pais</label>
+        <select
+          id="country"
+          name="country"
+          value={search.country}
+          onChange={handleChange}
+        >
+          <option value="">-- Seleccione un País--</option>
+          {countries.map((country) => (
+            <option key={country.code} value={country.code}>
+              {country.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <input className={styles.submit} type="submit" value="Consultar Clima" />
+    </form>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    port: 5000,
+    host: true,
+  },
+})
